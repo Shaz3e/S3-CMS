451b79e0f6827baef41cca97860b44bb6b00bc21
Shahrukh A. Khan <iam@shaz3e.com>
2018-09-06 19:39:16 +0500

    Changed to mysqli

--- s3tools/classes/database.php
+++ s3tools/classes/database.php
@@ -1,5 +1,8 @@
 <?php
-class MySQLDatabase{
+/**
+* Database Class
+*/
+class MySQLiDatabase{
 	var $db_host = ''; // database host
 	var $db_name = ''; // datbase name
 	var $db_user = ''; // database username
@@ -14,13 +17,13 @@ class MySQLDatabase{
 	
 	// connect to the database
 	public function connect(){
-		$this->connection = mysql_connect($this->db_host,$this->db_user,$this->db_pass);
+		$this->connection = mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
 		if(!$this->connection){
-			die ("Database connection failed: " . mysql_error());
+			die ("Database connection failed: " . mysqli_connect_error());
 		}else{
-			$select_db = mysql_select_db($this->db_name,$this->connection);
+			$select_db = mysqli_select_db($this->connection,$this->db_name);
 			if(!$select_db){
-				die("Database selection failed: " . mysql_error());
+				die("Database selection failed: " . mysqli_error($this->connection));
 			}
 		}
 	} // end connect()
@@ -28,14 +31,14 @@ class MySQLDatabase{
 	// close mysql connection
 	public function close_connection(){
 		if(isset($this->connection)){
-			mysql_close($this->connection);
+			mysqli_close($this->connection);
 			unset($this->connection);
 		}
 	} // end close_connection()
 	
 	// run database query
 	public function query($sql){
-		$result = mysql_query($sql);
+		$result = mysqli_query($this->connection,$sql);
 		$this->confirm_query($result);
 		return $result;
 	} // end query($sql)
@@ -43,22 +46,21 @@ class MySQLDatabase{
 	// confirm mysql query 
 	public function confirm_query($result){
 		if(!$result){
-			die ("Database query failed: " . mysql_error());
+			die ("Database query failed: " . mysqli_error($this->connection));
 		}
 	} // end confirm_query($result)
 	
 	// mysql fetch_array()
 	public function fetch_array($result_set){
-		return mysql_fetch_array($result_set);
+		return mysqli_fetch_array($result_set);
 	}// end fetch_array()
 		
 	// mysql num_rows()
 	public function num_rows($result_set){
-		return mysql_num_rows($result_set);
+		return mysqli_num_rows($result_set);
 	}// end num_rows()
 	
 } // end class MySQLDatabase
 
-$database = new MySQLDatabase();
+$database = new MySQLiDatabase();
 $db =& $database;
-?>
